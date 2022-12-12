148f7c4b1048f69764555fd7b413803dadcdf16f
l kswrr <253110376@qq.com>
2022-12-12 10:54:20 +0000

    update to main

--- config/my.php
+++ config/my.php
@@ -5,13 +5,13 @@ $config->requestType     = 'GET';
 $config->timezone        = 'Asia/Shanghai';
 $config->db->host        = '127.0.0.1';
 $config->db->port        = '3306';
-$config->db->name        = 'zentao';
-$config->db->user        = 'root';
+$config->db->name        = 'zentaopms_17_7';
+$config->db->user        = 'zentaopms_17_7_man';
 $config->db->encoding    = 'UTF8';
-$config->db->password    = '100200';
+$config->db->password    = '1234';
 $config->db->prefix      = 'zt_';
 $config->webRoot         = getWebRoot();
 $config->default->lang   = 'zh-cn';
-$config->customSession   = true;
+$config->customSession = true;
 
 
