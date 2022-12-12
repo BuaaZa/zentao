ab86c364368bc186e6e0069bdc0f9df2daeb1991
Mayday12138 <1297330224@qq.com>
2022-12-12 01:34:32 +0000

    Update teamaccount.php(修改team的两个方法合并)

--- api/v1/entries/teamaccount.php
+++ api/v1/entries/teamaccount.php
@@ -1,26 +1,71 @@
 <?php
 /**
-
+ * The team entry point of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2021 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     ZPL(http://zpl.pub/page/zplv12.html) or AGPL(https://www.gnu.org/licenses/agpl-3.0.en.html)
+ * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
+ * @package     entries
+ * @version     1
+ * @link        http://www.zentao.net
  */
-class teamAccountEntry extends Entry
+class teamAccountuserEntry extends Entry
 {
-    /**
-     * GET method.
-     *
-     * @param  int    $teamID
-     * @access public
-     * @return void
-     */
     
-
-    /**
-     * PUT method.
-     *
-     * @param  int    $teamID
-     * @access public
-     * @return void
-     */
-   
+    public function put($teamID){
+        $this->loadModel("team");
+        $team = $this->team->getTeam($teamID);
+        if(empty($team)){
+            $data["error"] = "No such team";
+            return $this->send(200, $this->format($data,""));
+        }
+        if(!isset($this->requestBody->account)){
+            $data["error"] = "Need account";
+            return $this->send(200, $this->format($data,""));
+        }
+        $account = $this->requestBody->account;
+        $user = $this->loadModel("user")->getById($account,'account');
+        if(!$user){
+            $data["error"] = "No such user";
+            return $this->send(200, $this->format($data,""));
+        }
+        $teamAccount = $this->team->getTeamAccount($teamID, $account);
+        if(!$teamAccount){
+            $fields = "role,position,limited,join,days,hours,estimate,consumed,left,order";
+            
+            $this->batchSetPost($fields);
+            $now = helper::today();
+            $fixData = fixer::input('post')
+            ->setDefault('days,hours,estimate,consumed,left,order', 0)
+            ->setDefault('role,position', "")
+            ->setDefault('limited', "no")
+            ->setDefault('join', $now)
+            ->add('root',$teamID)
+            ->add('type',$team[0]->type)
+            ->add('account',$account)
+            ->get(); 
+            $this->team->addAccount($fixData);
+            if(dao::isError()){
+                return $this->sendError(400, dao::getError());
+            }
+            $data["message"] = "success";
+            return $this->send(200, $this->format($data,""));
+        }else{
+            $fields = "role,position,limited,join,days,hours,estimate,consumed,left,order";
+            
+            $this->batchSetPost($fields, $teamAccount);
+            
+            $fixData = fixer::input('post')
+            ->remove('id,root,type,account')
+            ->get(); 
+            $this->team->updateTeamAccount($teamAccount->id, $fixData);
+            if(dao::isError()){
+                return $this->sendError(400, dao::getError());
+            }
+            $data["message"] = "success";
+            return $this->send(200, $this->format($data,""));       
+        }
+    }
 
     /**
      * DELETE method.
