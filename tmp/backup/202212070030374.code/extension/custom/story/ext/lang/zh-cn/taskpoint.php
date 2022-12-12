fa5f66c925e1d511d25a415ded321132dc9ed334
lkswrr <253110376@qq.com>
2022-12-12 19:35:00 +0800

    update .gitignore

--- tmp/backup/202212070030374.code/extension/custom/story/ext/lang/zh-cn/taskpoint.php
+++
@@ -1,4 +0,0 @@
-<?php
-$lang->story->createTaskPoint = "提功能点";
-$lang->story->taskPointName = "功能点名称"
-?>
--- tmp/backup/202212070030374.code/module/stage/config.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$config->stage->create = new stdclass();
-$config->stage->edit   = new stdclass();
-$config->stage->create->requiredFields = 'name,percent,type';
-$config->stage->edit->requiredFields   = 'name,percent,type';
