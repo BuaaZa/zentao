4b1b656e1d1337e7c88b9328896ed60de019cbe5
Duan Wenrui <isduanwenrui2001@163.com>
2022-12-13 09:53:57 +0800

    fix: 清空www/data/upload和tmp下残余文件

--- tmp/backup/202212070030374.code/module/ci/lang/fr.php
+++
@@ -1,8 +0,0 @@
-<?php
-$lang->ci->common             = 'CI';
-$lang->ci->commitResult       = 'Interface validation des r閟ultats de tests.';
-$lang->ci->checkCompileStatus = 'Interface: Fetch Test Result.';
-
-$lang->ci->job     = 'Construction';
-$lang->ci->task    = 'T鈉he';
-$lang->ci->history = 'Historique';
--- tmp/backup/202212070030374.code/module/dev/view/header.html.php
+++
@@ -1,5 +0,0 @@
-<?php include $app->getModuleRoot() . 'common/view/header.html.php';?>
-<div id='mainMenu' class='clearfix'>
-  <div class='btn-toolbar pull-left'><?php // common::printAdminSubMenu('dev');?></div>
-</div>
-<script>$('#mainMenu #<?php echo $tab;?>').addClass('btn-active-text')</script>
