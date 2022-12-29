73443e795fae538dc9ca0f5aeefcb37e89361320
罗 <1833935022@qq.com>
2022-12-29 19:08:19 +0800

    新增两个数据表，修改casestep数据表

--- db/myDbChange/allChange.sql
+++ db/myDbChange/allChange.sql
@@ -1,4 +1,7 @@
 # 添加数据库修改脚本
 # Linux 环境可在 updateDbServer.sh 定义好变量后执行
 # 其他环境可在db/myDbChange目录下执行 mysql -u ${user} -p${password} -D ${database} < allChange.sql
-source actionChange.sql
+source actionChange.sql;
+source zt_testcase_io.sql;
+source zt_data_sample.sql;
+source casestepChange.sql;
