de270f88051bf5519ff050a7d6f07d91e669a8ec
ao <1051869687@qq.com>
2023-01-12 03:00:16 +0000

    feature:动态归档 model

--- db/myDbChange/actionarchiveChange.sql
+++ db/myDbChange/actionarchiveChange.sql
@@ -1,4 +1,4 @@
-create table zt_actionarchive
+create table if not exists zt_actionarchive
 (
     id         int unsigned auto_increment
         primary key,
@@ -19,17 +19,30 @@ create table zt_actionarchive
 )
     charset = utf8mb3;
 
-create index action
-    on zt_actionarchive (action);
 
-create index actor
-    on zt_actionarchive (actor);
+DROP PROCEDURE IF EXISTS add_index;
+DELIMITER //
+CREATE PROCEDURE add_index(in target_index_name VARCHAR(100) , in target_column_name VARCHAR(100))
 
-create index date
-    on zt_actionarchive (date);
+BEGIN
+    DECLARE target_table_name VARCHAR(100);
+    set target_table_name = 'zt_actionarchive';
+    IF NOT EXISTS (
+        SELECT *
+        FROM information_schema.statistics
+        WHERE table_name = target_table_name
+          AND index_name = target_index_name)
+        THEN
+        set @statement = CONCAT('ALTER TABLE ', target_table_name, ' ADD INDEX ', target_index_name, '(', target_column_name, ')');
+        PREPARE STMT FROM @statement;
+        EXECUTE STMT;
+    END IF;
+END//
 
-create index objectID
-    on zt_actionarchive (objectID);
-
-create index project
-    on zt_actionarchive (project);
+DELIMITER ;
+CALL add_index('action','action');
+CALL add_index('actor','actor');
+CALL add_index('date','date');
+CALL add_index('objectID','objectID');
+CALL add_index('project','project');
+DROP PROCEDURE add_index;
--- db/zentao.sql
+++ db/zentao.sql
@@ -14294,7 +14294,7 @@ alter table `zt_effort`
     add `syncStatus` enum ('0', '1') default '0' not null after `work`;
 
 # zt_actionarchive
-create table `zt_actionarchive`
+create table if not exists `zt_actionarchive`
 (
     id         int unsigned auto_increment
         primary key,
