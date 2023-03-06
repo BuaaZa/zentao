c626d89b9af42820f06b033b460f7dd22d571353
lkswrr <253110376@qq.com>
2023-03-06 15:50:14 +0800

    testtask更新zentao.sql

--- db/zentao.sql
+++ db/zentao.sql
@@ -14332,3 +14332,20 @@ create index project
 
 alter table zt_task
     add workcodelines int unsigned default 0 null after fromIssue;
+
+DELIMITER //
+CREATE PROCEDURE addColumnAtTesttask()
+
+BEGIN
+    IF NOT EXISTS(SELECT * FROM information_schema.COLUMNS
+                  WHERE COLUMN_NAME = 'parent' AND TABLE_NAME = 'zt_testtask')
+    THEN
+        alter table `zt_testtask` add `parent` int not null;
+    END IF;
+END//
+
+DELIMITER ';'
+
+CALL addColumnAtTesttask();
+
+DROP PROCEDURE addColumnAtTesttask;
