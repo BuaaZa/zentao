d7c8afe6fc283bda8a7cdb9839455b8ed0ff287c
dwr2001 <isduanwenrui2001@163.com>
2023-03-13 14:03:06 +0800

    chore: 删除迅捷版本代码扩展

--- extension/lite/kanban/ext/view/view.lite.html.hook.php
+++
@@ -1,7 +0,0 @@
-<script>
-<?php if(trim($config->visions, ',') == 'lite'): ?>
-$('#headerActions').css("right", '110px');
-<?php else: ?>
-$('#headerActions').css("right", '205px');
-<?php endif; ?>
-</script>
