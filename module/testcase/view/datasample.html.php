08c81dbcac947f3599096f9a2157209a024577d5
罗 <1833935022@qq.com>
2023-03-20 17:43:36 +0800

    feat:使数据样本表填写页的样式禅道化

--- module/testcase/view/datasample.html.php
+++ module/testcase/view/datasample.html.php
@@ -6,14 +6,15 @@
         <h2>填写数据样本</h2>
     </div>
     <div style="text-align:center">
-    <table id="testTable" border="1" align="center">
+    <table id="testTable" class='table table-form mg-0 table-bordered' style='border: 1px solid #ddd'>
         <tr>
-            <td>输入输出项</td>
-            <td onmouseover="mover(1);" onmouseout="mout(1);">样本1</td>
+            <td><b>输入输出项名称</b></td>
+            <td><b>样本1</b></td>
         </tr>
     </table>
-    <input type="button" value="添加输入项" onclick="addRow();"/>
-    <input type="button" value="添加样本" onclick="addCol();"/>
+        <button class="btn" type="button"  onclick="addRow();"> 添加输入输出项</button>
+        <button class="btn" type="button"  onclick="addCol();"> 添加测试样本</button>
+        <button class="btn btn-wide btn-primary" type="button"  onclick="save_in_cookie();"> 保存</button>
     <!-- <input type="button" value="保存" onclick="save_in_cookie();"/> -->
     </div>
 </div>
@@ -32,10 +33,10 @@
         function addRow(){
 
             rowCount++;
-            var rowTemplate_1 = '<tr class="tr_'+rowCount+'"><td>'+rowCount+'</td>';
+            var rowTemplate_1 = "<td><textarea rows='1' class='form-control autosize step-expects' name='datasample[][]' placeholder=''></textarea></td>";
             var rowTemplate_2 = '';
             for(i = 1; i<=colCount; i++){
-                var tmp_template = '<td class="cl1">样本'+i+'</td>';
+                var tmp_template = "<td><textarea rows='1' class='form-control autosize step-expects' name='datasample[][]'></textarea></td>";
                 rowTemplate_2 += tmp_template;
             }
             var rowTemplate_3 = '</tr>';
@@ -52,10 +53,17 @@
         }
         function addCol(){
             colCount++;
+            var i = 1;
             $("#testTable tr").each(function(){
                 var trHtml = $(this).html();
-                trHtml += '<td onclick="delCol('+colCount+')">样本'+colCount+'</td>';
+                if(i === 1){
+                    trHtml += '<td onclick="delCol('+colCount+')"><b>样本'+colCount+'</b></td>';
+                }else{
+                    trHtml += "<td><textarea rows='1' class='form-control autosize step-expects' name='datasample[][]'></textarea></td>";
+                }
+
                 $(this).html(trHtml);
+                i++;
             });
         }
         function delCol(_id){
@@ -77,7 +85,7 @@
             });
         }
         function save_in_cookie(){
-            history.go(-1)
+            //history.go(-1)
         }
     </script>
 
