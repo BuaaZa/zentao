d031dcccbab345451bf22e8c2eca782312c6af84
Duan Wenrui <isduanwenrui2001@163.com>
2023-04-04 14:42:24 +0800

    feature: 个人档案信息屏蔽

--- module/my/view/profile.html.php
+++ module/my/view/profile.html.php
@@ -47,25 +47,25 @@
                     <td title='<?php echo $user->email; ?>'><?php echo html::mailto($user->email); ?></td>
                 </tr>
                 <tr>
-                    <th><?php echo $lang->user->dept; ?></th>
-                    <td>
-                        <?php
-                        if (empty($deptPath)) {
-                            echo "/";
-                        } else {
-                            foreach ($deptPath as $key => $dept) {
-                                if ($dept->name) echo $dept->name;
-                                if (isset($deptPath[$key + 1])) echo $lang->arrow;
-                            }
-                        }
-                        ?>
-                    </td>
-                    <th><?php echo $lang->user->role; ?></th>
-                    <td><?php echo zget($lang->user->roleList, $user->role, ''); ?></td>
+<!--                    <th>--><?php //echo $lang->user->dept; ?><!--</th>-->
+<!--                    <td>-->
+<!--                        --><?php
+//                        if (empty($deptPath)) {
+//                            echo "/";
+//                        } else {
+//                            foreach ($deptPath as $key => $dept) {
+//                                if ($dept->name) echo $dept->name;
+//                                if (isset($deptPath[$key + 1])) echo $lang->arrow;
+//                            }
+//                        }
+//                        ?>
+<!--                    </td>-->
+<!--                    <th>--><?php //echo $lang->user->role; ?><!--</th>-->
+<!--                    <td>--><?php //echo zget($lang->user->roleList, $user->role, ''); ?><!--</td>-->
                 </tr>
                 <tr>
-                    <th><?php echo $lang->user->joinAB; ?></th>
-                    <td><?php echo formatTime($user->join); ?></td>
+<!--                    <th>--><?php //echo $lang->user->joinAB; ?><!--</th>-->
+<!--                    <td>--><?php //echo formatTime($user->join); ?><!--</td>-->
                     <th><?php echo $lang->user->priv; ?></th>
                     <td><?php foreach ($groups as $group) echo $group->name . ' '; ?></td>
                 </tr>
@@ -74,46 +74,46 @@
                 <tr>
                     <th><?php echo $lang->user->mobile; ?></th>
                     <td><?php echo $user->mobile; ?></td>
-                    <th><?php echo $lang->user->weixin; ?></th>
-                    <td><?php echo $user->weixin; ?></td>
-                </tr>
-                <tr>
                     <th><?php echo $lang->user->phone; ?></th>
                     <td><?php echo $user->phone; ?></td>
-                    <th><?php echo $lang->user->qq; ?></th>
-                    <td><?php echo $user->qq; ?></td>
                 </tr>
-                <tr>
-                    <th><?php echo $lang->user->zipcode; ?></th>
-                    <td><?php echo $user->zipcode; ?></td>
-                    <th><?php echo $lang->user->addressAB; ?></th>
-                    <td title='<?php echo $user->address; ?>'><?php echo $user->address; ?></td>
-                </tr>
+<!--                <tr>-->
+<!--                    <th>--><?php //echo $lang->user->weixin; ?><!--</th>-->
+<!--                    <td>--><?php //echo $user->weixin; ?><!--</td>-->
+<!--                    <th>--><?php //echo $lang->user->qq; ?><!--</th>-->
+<!--                    <td>--><?php //echo $user->qq; ?><!--</td>-->
+<!--                </tr>-->
+<!--                <tr>-->
+<!--                    <th>--><?php //echo $lang->user->zipcode; ?><!--</th>-->
+<!--                    <td>--><?php //echo $user->zipcode; ?><!--</td>-->
+<!--                    <th>--><?php //echo $lang->user->addressAB; ?><!--</th>-->
+<!--                    <td title='--><?php //echo $user->address; ?><!--'>--><?php //echo $user->address; ?><!--</td>-->
+<!--                </tr>-->
             </table>
             <table>
                 <tr>
                     <th><?php echo $lang->user->commiter; ?></th>
                     <td><?php echo $user->commiter; ?></td>
-                    <th><?php echo $lang->user->skype; ?></th>
-                    <td><?php if ($user->skype) echo html::a("callto://$user->skype", $user->skype); ?></td>
+<!--                    <th>--><?php //echo $lang->user->skype; ?><!--</th>-->
+<!--                    <td>--><?php //if ($user->skype) echo html::a("callto://$user->skype", $user->skype); ?><!--</td>-->
                 </tr>
                 <tr>
                     <th><?php echo $lang->user->visits; ?></th>
                     <td><?php echo $user->visits; ?></td>
-                    <th><?php echo $lang->user->whatsapp; ?></th>
-                    <td><?php echo $user->whatsapp; ?></td>
+<!--                    <th>--><?php //echo $lang->user->whatsapp; ?><!--</th>-->
+<!--                    <td>--><?php //echo $user->whatsapp; ?><!--</td>-->
                 </tr>
                 <tr>
                     <th><?php echo $lang->user->last; ?></th>
                     <td><?php echo $user->last; ?></td>
-                    <th><?php echo $lang->user->slack; ?></th>
-                    <td><?php echo $user->slack; ?></td>
+<!--                    <th>--><?php //echo $lang->user->slack; ?><!--</th>-->
+<!--                    <td>--><?php //echo $user->slack; ?><!--</td>-->
                 </tr>
                 <tr>
                     <th><?php echo $lang->user->ip; ?></th>
                     <td><?php echo $user->ip; ?></td>
-                    <th><?php echo $lang->user->dingding; ?></th>
-                    <td><?php echo $user->dingding; ?></td>
+<!--                    <th>--><?php //echo $lang->user->dingding; ?><!--</th>-->
+<!--                    <td>--><?php //echo $user->dingding; ?><!--</td>-->
                 </tr>
             </table>
         </div>
