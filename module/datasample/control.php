8fb82baad2d7831e295cfb03e1de4494c9996887
Craze-H <70187423+Craze-H@users.noreply.github.com>
2023-04-20 17:33:54 +0800

    datasample的mockInteger

--- module/datasample/control.php
+++ module/datasample/control.php
@@ -30,4 +30,59 @@ class datasample extends control
         return 0;
     }
 
+    public function parseStrMock($mock = ''){
+        $match = preg_match('/^\$(\w+)\(.*\)$/', $mock, $matches);
+        if ($match) {
+            $funcName = $matches[1];
+
+            if ($funcName) {
+                $paramStr = preg_replace('/^\$\w+\(|\)$/', '', $mock);
+                preg_match_all('/("[^"]*"|\'[^\']*\'|\{[^}]*\}|\[[^\]]*\]|[^,]+)+/', $paramStr, $params);
+            } else {
+                $match = preg_match('/^\$(\w+)$/', $mock, $matches);
+                if ($match) {
+                $funcName = $matches[1];
+                }
+            }
+
+            $funcName = ucfirst(strtolower($funcName));
+        }
+        
+    }
+
+    public function mockInteger($data = '', $return = false){
+        $response = array();
+        $response['id'] = $data['id'];
+
+        $args = $this->ztinterface->parseParams($data["params"]);
+        $min = -65535;
+        $max = 65535;
+
+        if(isset($args[0]) and is_numeric($args[0])){
+            $min = (int)$args[0];
+            if($min != (float)$args[0]){
+                $min = $min+1;
+            }
+        }
+        if(isset($args[1]) and is_numeric($args[1])){
+            $max = (int)$args[1];
+        }
+
+        if($min>$max){
+            $temp = $min;
+            $min = $max;
+            $max = $temp;
+        }
+
+        $faker = $this->ztinterface->getFaker('en_US');
+        $response['value'] = $faker->numberBetween($min,$max);
+
+        if($return){
+            return $response;
+        }else{
+            echo json_encode($response);
+            return;
+        }
+    }
+
 }
