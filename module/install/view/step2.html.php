3c02e75a2d3f4fe7dd1c32d7791d4db1fa0bfa41
Duan Wenrui <isduanwenrui2001@163.com>
2023-04-21 11:09:00 +0800

    feature: 安装支持多种方式配置数据库

--- module/install/view/step2.html.php
+++ module/install/view/step2.html.php
@@ -8,67 +8,94 @@
  * @package     ZenTaoPMS
  * @version     $Id: step2.html.php 4972 2013-07-02 06:50:10Z zhujinyonging@gmail.com $
  */
-?>
-<?php include '../../common/view/header.lite.html.php';?>
+
+include '../../common/view/header.lite.html.php'; ?>
 <div class='container'>
-  <div class='modal-dialog'>
-    <form method='post' action='<?php echo $this->createLink('install', 'step3');?>'>
-      <div class='modal-header'><strong><?php echo $lang->install->setConfig;?></strong></div>
-      <div class='modal-body'>
-        <table align='center' class='table table-bordered table-form'>
-          <thead>
-            <tr>
-              <th class='w-p20 text-right'><?php echo $lang->install->key;?></th>
-              <th colspan='2'><?php echo $lang->install->value?></th>
-            </tr>
-          </thead>
-          <tr>
-            <?php include $this->app->getConfigRoot() . 'timezones.php';?>
-            <th><?php echo $lang->install->timezone;?></th>
-            <td><?php echo html::select('timezone', $timezoneList, $config->timezone, "class='form-control'");?></td><td></td>
-          </tr>
-          <tr>
-            <th><?php echo $lang->install->defaultLang;?></th>
-            <td><?php echo html::select('defaultLang', $config->langs, $app->getClientLang(), "class='form-control'");?></td><td></td>
-          </tr>
-          <tr>
-            <th><?php echo $lang->install->dbHost;?></th>
-            <td><?php echo html::input('dbHost', $dbHost, "class='form-control'");?></td>
-            <td><?php echo $lang->install->dbHostNote;?></td>
-          </tr>
-          <tr>
-            <th><?php echo $lang->install->dbPort;?></th>
-            <td><?php echo html::input('dbPort', $dbPort, "class='form-control'");?></td><td></td>
-          </tr>
-          <tr>
-            <th><?php echo $lang->install->dbEncoding;?></th>
-            <td><?php echo html::input('dbEncoding', $this->config->db->encoding, "class='form-control'");?></td><td></td>
-          </tr>
-          <tr>
-            <th><?php echo $lang->install->dbUser;?></th>
-            <td><?php echo html::input('dbUser', $dbUser, "class='form-control'");?></td><td></td>
-          </tr>
-          <tr>
-            <th><?php echo $lang->install->dbPassword;?></th>
-            <td><?php echo html::input('dbPassword', $dbPassword, "class='form-control'");?></td><td></td>
-          </tr>
-          <tr>
-            <th><?php echo $lang->install->dbName;?></th>
-            <td><?php echo html::input('dbName', $dbName, "class='form-control'");?></td><td></td>
-          </tr>
-          <tr>
-            <th><?php echo $lang->install->dbPrefix;?></th>
-            <td>
-            <?php echo html::input('dbPrefix', 'zt_', "class='form-control'");?>
-            </td>
-            <td><?php echo html::checkBox('clearDB', $lang->install->clearDB);?></td>
-          </tr>
-        </table>
-      </div>
-      <div class='modal-footer'>
-        <?php echo html::submitButton() . html::hidden('requestType', 'GET');?>
-      </div>
-    </form>
-  </div>
+    <div class='modal-dialog'>
+        <form method='post' action='<?php echo $this->createLink('install', 'step3'); ?>'>
+            <div class='modal-header'><strong><?php echo $lang->install->setConfig; ?></strong></div>
+            <div class='modal-body'>
+                <table align='center' class='table table-bordered table-form'>
+                    <thead>
+                    <tr>
+                        <th class='w-p20 text-right'><?php echo $lang->install->key; ?></th>
+                        <th style="text-align: center"><?php echo $lang->install->value ?></th>
+                        <th style="text-align: center">备注</th>
+                    </tr>
+                    </thead>
+                    <tr>
+                        <?php include $this->app->getConfigRoot() . 'timezones.php'; ?>
+                        <th><?php echo $lang->install->timezone; ?></th>
+                        <td><?php echo html::select('timezone', $timezoneList, $config->timezone, "class='form-control'"); ?></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th><?php echo $lang->install->defaultLang; ?></th>
+                        <td><?php echo html::select('defaultLang', $config->langs, $app->getClientLang(), "class='form-control'"); ?></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th><?php echo $lang->install->dbHost; ?></th>
+                        <td>
+                            <?php echo html::input('dbHost', $dbHost, "class='form-control'"); ?>
+                        </td>
+                        <td><?php echo $lang->install->dbHostNote; ?></td>
+                    </tr>
+                    <tr>
+                        <th><?php echo $lang->install->dbPort; ?></th>
+                        <td>
+                            <?php echo html::input('dbPort', $dbPort, "class='form-control'"); ?>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th><?php echo $lang->install->dbEncoding; ?></th>
+                        <td>
+                            <?php echo html::input('dbEncoding', $this->config->db->encoding, "class='form-control'"); ?>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th><?php echo $lang->install->dbUser; ?></th>
+                        <td>
+                            <?php echo html::input('dbUser', $dbUser, "class='form-control'"); ?>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th><?php echo $lang->install->dbPassword; ?></th>
+                        <td>
+                            <?php echo html::input('dbPassword', $dbPassword, "class='form-control'", false, 'password'); ?>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th><?php echo $lang->install->dbName; ?></th>
+                        <td>
+                            <?php echo html::input('dbName', $dbName, "class='form-control'"); ?>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th><?php echo $lang->install->dbPrefix; ?></th>
+                        <td>
+                            <?php echo html::input('dbPrefix', 'zt_', "class='form-control'"); ?>
+                        </td>
+                        <td><?php echo html::checkBox('clearDB', $lang->install->clearDB); ?></td>
+                    </tr>
+                    <tr>
+                        <th><?php echo $lang->install->installType; ?></th>
+                        <td>
+                            <?php echo html::select('installType', $lang->install->installTypes, 'install', "class='form-control'"); ?>
+                        </td>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+            <div class='modal-footer'>
+                <?php echo html::submitButton() . html::hidden('requestType', 'GET'); ?>
+            </div>
+        </form>
+    </div>
 </div>
-<?php include '../../common/view/footer.lite.html.php';?>
+<?php include '../../common/view/footer.lite.html.php'; ?>
