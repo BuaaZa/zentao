33155cfb376097ab67e391cc8c9ae54c21547d87
Craze-H <70187423+Craze-H@users.noreply.github.com>
2023-04-25 11:30:22 +0800

    修改功能点列表“操作”栏宽度

--- module/qastory/config.php
+++ module/qastory/config.php
@@ -255,7 +255,7 @@ $config->story->datatable->fieldList['caseCount']['name']     = $lang->story->ca
 
 $config->story->datatable->fieldList['actions']['title']    = 'actions';
 $config->story->datatable->fieldList['actions']['fixed']    = 'right';
-$config->story->datatable->fieldList['actions']['width']    = $app->tab == 'project' ? '220' : '210';
+$config->story->datatable->fieldList['actions']['width']    = $app->tab == 'project' ? '220' : ($app->tab == 'qa' ? '90' : '210');
 $config->story->datatable->fieldList['actions']['required'] = 'yes';
 
 $config->story->fromObjects=array();
